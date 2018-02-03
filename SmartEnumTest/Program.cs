3e36fbb7885c928413a436a9591f493c27d95291
Benjamin Scabbia <benscabbia@gmail.com>
2018-02-03 22:04:22 +0000

    added source code for smarter enumeration blog post

---
+++ SmartEnumTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SmartEnumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Original
+            Console.WriteLine(CryptoEnum.Bitcoin.GetShortName());
+            Console.WriteLine(CryptoEnum.Bitcoin.GetDisplayName());
+
+            // Smart Enum
+            Console.WriteLine(SmartCryptoEnum.Bitcoin.ShortName);
+            Console.WriteLine(SmartCryptoEnum.Bitcoin.Name);
+
+            Console.WriteLine(SmartCryptoEnum.Bitcoin.WebsiteURL);
+            Console.WriteLine(SmartCryptoEnum.Bitcoin == SmartCryptoEnum.Dogecoin);
+            
+            // Get all registered coins
+            SmartCryptoEnum.List.OrderBy(x=> x.Value).ToList().ForEach(x => Console.WriteLine($"{x.Name} : {x.Value}"));
+
+            Console.ReadKey();
+        }
+    }
+}
